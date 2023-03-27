3e4569427893435f697288280e2dc05deb16edb2
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-03-28 01:24:30 +0800

    Add W1-04.py by commands

---
+++ W1/W1-04.py
@@ -0,0 +1,17 @@
+#
+git config --global user.email "me@example.com"
+#
+git config --global user.name "Myname"
+#create a new git
+git init
+# see detail
+ls -l .git/
+
+cp ../disk_usage.py .
+ls -l
+
+git add disk_usage.py
+
+git status
+
+git commit
