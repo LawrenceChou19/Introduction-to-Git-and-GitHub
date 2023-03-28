6e6d9c6c2e06ff4eb856218ef6767329208e1896
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-03-28 11:49:21 +0800

    test

--- W1/W1-04.py
+++ W1/W1-04.py
@@ -1,17 +1,26 @@
-#
-git config --global user.email "me@example.com"
-#
-git config --global user.name "Myname"
-#create a new git
-git init
-# see detail
-ls -l .git/
+# #
+# git config --global user.email "me@example.com"
+# #
+# git config --global user.name "Myname"
+# #create a new git
+# git init
+# # see detail
+# ls -l .git/
 
-cp ../disk_usage.py .
-ls -l
+# cp ../disk_usage.py .
+# ls -l
 
-git add disk_usage.py
+# git add disk_usage.py
 
-git status
+# git status
 
+# git commit
+
+#!/usr/bin/env python3
+def main():
+    pass
+main()
+
+git status()
+git add W1-04.py
 git commit
