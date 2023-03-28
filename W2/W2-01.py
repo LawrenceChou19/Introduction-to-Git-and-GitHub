8bb6529156691471f642cc41492bf00c6396393a
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-03-28 19:04:42 +0800

    adding the code

--- W2/W2-01.py
+++ W2/W2-01.py
@@ -2,3 +2,12 @@ git commit -a -m "Call check_reboot from main, exit with 1 on error"
 git log -p
 git show "Specific ID 439edd244560b18b89ae8ba90361b8984b893c4d"
 git log --stat
+
+git rm all_checks.py
+git mv all.py check_free_space.py
+
+.gitignore
+echo .DS_STORE > .gitignore
+ls -la
+git add .gitignore
+git commit -m 'Add a gitignore file, ignoring .DS_STORE files'
