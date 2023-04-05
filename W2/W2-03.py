8d5b9a4893e99c4f83e3e378fc2b60e76329e82c
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-04-05 20:48:22 +0800

    Print everything is ok

---
+++ W2/W2-03.py
@@ -0,0 +1,20 @@
+git branch
+git branch new-feature
+git checkout
+git checkout new-feature
+
+
+git status
+
+ls -l
+git checkout master
+
+git log -2
+ 
+git branch
+
+git branch -d new-feature
+git branch -d even-better-feature
+
+
+git merge even-better-feature
