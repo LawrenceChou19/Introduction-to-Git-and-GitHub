64a39cc2c61e3cf12ae96a3ee4c80733b1745275
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-04-09 14:56:55 +0800

    2023-04-09

---
+++ W3/W3-01.py
@@ -0,0 +1,19 @@
+git clone
+username
+password
+git push
+git remote -v
+
+git remote show origin
+git branch -r
+
+git status
+
+git log origin/main
+git merge origin/main
+
+
+git log -p -1
+git checkout experimental
+
+git log -p origin/master
